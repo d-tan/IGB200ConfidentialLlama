78cca6d2c3dbf3e8c0d29b2065f96701088e9c01
Daniel Tan <danieltan843@gmail.com>
2017-08-28 00:24:51 +1000

    Added some base to start controls

---
+++ Assets/DannyRealm/Scripts/TouchControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControls : MonoBehaviour {
+
+	// Touch
+
+
+	// Mouse
+	RaycastHit m_RayCast;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Touch ();
+		Mouse ();
+	}
+
+	void Touch() {
+		
+	}
+
+	void Mouse() {
+		if (Input.GetMouseButton(0)) {
+//			Physics.Raycast (Camera.main.ScreenToWorldPoint (Input.mousePosition), out m_RayCast);
+
+			if (m_RayCast.transform != null) {
+				
+			}
+
+		} else if (Input.GetMouseButtonUp(0)) {
+			
+		}
+	}
+}
