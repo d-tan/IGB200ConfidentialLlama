6de992078c9740129c8dff22956ce3ff3e4692ef
Daniel Tan <danieltan843@gmail.com>
2017-09-07 00:08:04 +1000

    Sort of have flicking

--- Assets/DannyRealm/Scripts/TouchControls.cs
+++ Assets/DannyRealm/Scripts/TouchControls.cs
@@ -4,48 +4,71 @@ using UnityEngine;
 
 public class TouchControls : MonoBehaviour {
 
+	public bool mouseControl = false;
+
 	// Touch
-	const int numTouches = 4; 
+	const int numTouches = 4;
+	Touch currentTouch;
 	RaycastHit[] t_Raycasts = new RaycastHit[numTouches];
-	GameObject[] t_HeldObjects = new GameObject[numTouches];
+	public GameObject[] t_HeldObjects = new GameObject[numTouches];
 	Vector3[] t_fingerPos = new Vector3[numTouches];
+	bool[] t_stationary = new bool[numTouches];
+	Vector3 resetPos;
+
+	// Flick
+	Vector3[] t_velocity = new Vector3[numTouches];
+	Vector3[] t_prevPos = new Vector3[numTouches];
+	float minVelSqr = 1.5f;
+	float flickVelMultiplier = 7f;
 
 	// Mouse
 	RaycastHit m_RayCast;
 	GameObject m_HeldObject;
 	Vector3 m_mousePos;
+
+	void Start() {
+		for (int i = 0; i < t_stationary.Length; i++) {
+			t_stationary [i] = true;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (mouseControl)
+			Mouse ();
+		
 		Touch ();
-		Mouse ();
 
 	}
 
 	void Touch() {
 
-		foreach (Touch touch in Input.touches) {
-			
-			if (touch.fingerId < numTouches) {
+//		foreach (Touch touch in Input.touches) {
+		int touchCount = Input.touchCount;
+		for (int i = 0; i < touchCount; i++) {
+			currentTouch = Input.GetTouch (i);
+			if (currentTouch.fingerId < numTouches) {
 				
-				switch (touch.phase) {
+				switch (currentTouch.phase) {
 				case TouchPhase.Began:
-					InitialiseTouch (touch);
+					InitialiseTouch (currentTouch);
 					break;
 
 				case TouchPhase.Moved:
-					DragTouch (touch);
+					DragTouch (currentTouch);
 					break;
 
 				case TouchPhase.Stationary:
+					StationaryTouch (currentTouch);
 					break;
 
 				case TouchPhase.Ended:
-					FinishTouch (touch);
+					FinishTouch (currentTouch);
 					break;
 
 				case TouchPhase.Canceled:
-					FinishTouch (touch);
+					FinishTouch (currentTouch);
 					break;
 				}
 			}
@@ -63,7 +86,19 @@ public class TouchControls : MonoBehaviour {
 
 			if (t_HeldObjects[touch.fingerId] == null && t_Raycasts[touch.fingerId].transform.CompareTag("PickUp")) {
 
-				t_HeldObjects [touch.fingerId] = t_Raycasts [touch.fingerId].transform.gameObject;
+				if (!CheckAlreadyHolding (t_Raycasts [touch.fingerId].transform.gameObject)) {
+					// Store gameObject
+					t_HeldObjects [touch.fingerId] = t_Raycasts [touch.fingerId].transform.gameObject;
+
+					// Zero the velocity
+					t_HeldObjects [touch.fingerId].GetComponent<Rigidbody> ().velocity = Vector3.zero;
+
+					// Change the local scale
+					t_HeldObjects [touch.fingerId].transform.localScale *= 2;
+
+					// Store the position for calculating velocity
+					t_prevPos [touch.fingerId] = t_HeldObjects [touch.fingerId].transform.position;
+				}
 			}
 		}
 	}
@@ -78,7 +113,24 @@ public class TouchControls : MonoBehaviour {
 			t_fingerPos [touch.fingerId].y = 0;
 
 			t_HeldObjects [touch.fingerId].transform.position = t_fingerPos [touch.fingerId];
+
+			if (t_stationary [touch.fingerId])
+				t_stationary [touch.fingerId] = false;
+
+			CalculateVelocity (touch.fingerId, t_fingerPos[touch.fingerId]);
 		}
+	}
+
+	void StationaryTouch(Touch touch) {
+		if (!t_stationary [touch.fingerId])
+			t_stationary [touch.fingerId] = true;
+		
+		CalculateVelocity (touch.fingerId, t_fingerPos[touch.fingerId]);
+	}
+
+	void CalculateVelocity(int touchId, Vector3 pos) {
+		t_velocity [touchId] = pos - t_prevPos [touchId];
+		t_prevPos[touchId] = pos;
 	}
 
 	/// <summary>
@@ -87,9 +139,30 @@ public class TouchControls : MonoBehaviour {
 	/// <param name="touch">Touch.</param>
 	void FinishTouch(Touch touch) {
 		if (t_HeldObjects[touch.fingerId] != null) {
+			t_HeldObjects [touch.fingerId].transform.localScale = Vector3.one;
+
+			if (t_velocity[touch.fingerId].sqrMagnitude > minVelSqr) {
+				t_HeldObjects [touch.fingerId].GetComponent<Rigidbody> ().velocity = t_velocity [touch.fingerId] * flickVelMultiplier;
+			}
+
+			Debug.Log ("Flick: " + !t_stationary [touch.fingerId] + " counted: " + (t_velocity[touch.fingerId].sqrMagnitude > minVelSqr) + " vel: " + t_velocity[touch.fingerId].sqrMagnitude);
+
+
+			if (!t_stationary [touch.fingerId])
+				t_stationary [touch.fingerId] = true;
 
 			t_HeldObjects [touch.fingerId] = null;
 		}
+	}
+
+	bool CheckAlreadyHolding(GameObject obj) {
+		for (int i = 0; i < t_HeldObjects.Length; i++) {
+
+			if (obj.Equals (t_HeldObjects [i]))
+				return true;
+		}
+
+		return false;
 	}
 
 	/// <summary>
