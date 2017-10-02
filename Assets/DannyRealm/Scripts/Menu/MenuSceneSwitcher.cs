1ddeaf418ff166876b17a4c6b7ae88b9deba2ebf
Hayden Edge <Hayden_edge@hotmail.com>
2017-10-02 22:45:40 +1000

    Menu | Pause | Return to Menu

---
+++ Assets/DannyRealm/Scripts/Menu/MenuSceneSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneSwitcher : MonoBehaviour {
+
+	public bool isStart;
+	public bool isQuit;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseUp() {
+		if (isStart) {
+			SceneManager.LoadScene (1);
+			Debug.Log ("LoadLevel");
+		}
+		if (isQuit) {
+			Application.Quit ();
+			Debug.Log ("Quit");
+		}
+	}
+}
---
+++ Assets/DannyRealm/Scripts/Menu/PauseResume.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseResume : MonoBehaviour {
+
+	public bool paused = false;
+	public TouchControls controls;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	public void PauseGame() {
+		if (paused == false) {
+			Time.timeScale = 0.0f;
+			controls.enabled = false;
+			paused = true;
+		} else if (paused == true) {
+			Time.timeScale = 1.0f;
+			controls.enabled = true;
+			paused = false;
+		}
+	}
+
+	public void QuitToMenu() {
+		SceneManager.LoadScene (0);
+	}
+}
