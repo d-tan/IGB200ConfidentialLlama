ca856157d6fb6e9353d423196ac71c14a96fab00
Hayden Edge <Hayden_edge@hotmail.com>
2017-10-15 15:50:52 +1000

    Timer Implemented

---
+++ Assets/DannyRealm/Scripts/TimerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+
+	public Text timerText;
+	public float gametime = 300.0f;
+
+	// Use this for initialization
+	void Start () {
+		SetGameTime ();
+		InvokeRepeating ("IncrementCounter", 1.0f, 1.0f);
+	}
+
+	void SetGameTime() {
+		timerText.text = gametime.ToString();
+	}
+
+	void IncrementCounter() {
+		if (gametime > 0.0f) {
+			gametime = gametime - 1.0f;
+			gametime = Mathf.Round (gametime * 100f) / 100f;
+			SetGameTime ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gametime <= 0.0f) { // When time runs out...
+			// Show score screen
+		}
+	}
+}
